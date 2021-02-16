e2506b23202cf5209978d22f19900a558548b874
Asif-newaz <asif.newaz0093@gmail.com>
2021-02-16 17:01:27 +0600

    comment out some code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 
 import "./App.css";
-import randomcolor from "randomcolor";
+
+// import randomcolor from "randomcolor";
 // import Axios from "axios";
 
 function App() {
   // const [joke, setJoke] = useState("");
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState(randomcolor());
+
+  // const [count, setCount] = useState(0);
+  // const [color, setColor] = useState(randomcolor());
 
   // useEffect(() => {
   //   effect
@@ -16,15 +18,15 @@ function App() {
   //   }
   // }, [input])
 
-  useEffect(() => {
-    setInterval(() => {
-      setCount((prevCount) => prevCount + 1);
-    }, 1000);
-  }, []);
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     setCount((prevCount) => prevCount + 1);
+  //   }, 1000);
+  // }, []);
 
-  useEffect(() => {
-    setColor(randomcolor());
-  }, [count]);
+  // useEffect(() => {
+  //   setColor(randomcolor());
+  // }, [count]);
 
   // const getJoke = () => {
   //   Axios.get("https://official-joke-api.appspot.com/random_joke").then(
@@ -46,7 +48,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1 style={{ color: color }}>Hello useState!</h1>
+      {/* <h1 style={{ color: color }}>Hello useState!</h1>
       <h1
         style={{
           color: color,
@@ -57,7 +59,8 @@ function App() {
         }}
       >
         {count}
-      </h1>
+      </h1> */}
+
       {/* <h1>Hello API!</h1> */}
       {/* <q style={{ display: "block", marginBottom: "1rem" }}>{joke}</q> */}
       {/* <button onClick={getJoke}>Get Joke!</button> */}
