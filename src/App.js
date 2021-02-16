41777dc828d78e855c3ffbb9b41bf91849dfbc2c
Asif-newaz <asif.newaz0093@gmail.com>
2021-02-16 18:42:14 +0600

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 // Components
 // import RandomJokeApiCall from "./Components/RandomJokeApiCall/index";
-import LearnUseEffectScrimba from "./Components/LearnUseEffectScrimba/index";
+// import LearnUseEffectScrimba from "./Components/LearnUseEffectScrimba/index";
+import LearnHooksSimplifiedWeb from "./Components/LearnHooksSimplifiedWeb/index";
 
 function App() {
   return (
     <div className="App">
-      <LearnUseEffectScrimba />
+      <LearnHooksSimplifiedWeb />
+      {/* <LearnUseEffectScrimba /> */}
       {/* <RandomJokeApiCall /> */}
     </div>
   );
