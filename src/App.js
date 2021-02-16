9b89d0a288fbbadf70075d6c4b3ff626f5f3ecf7
Asif-newaz <asif.newaz0093@gmail.com>
2021-02-16 17:40:30 +0600

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { useState, useEffect } from "react";
-
+import React from "react";
 import "./App.css";
 
+// Components
+import RandomJokeApiCall from "./Components/RandomJokeApiCall/index";
+
 // import randomcolor from "randomcolor";
-// import Axios from "axios";
 
 function App() {
-  // const [joke, setJoke] = useState("");
-
   // const [count, setCount] = useState(0);
   // const [color, setColor] = useState(randomcolor());
 
@@ -28,24 +27,6 @@ function App() {
   //   setColor(randomcolor());
   // }, [count]);
 
-  // const getJoke = () => {
-  //   Axios.get("https://official-joke-api.appspot.com/random_joke").then(
-  //     (response) => {
-  //       setJoke(`${response.data.setup} ... ${response.data.punchline}`);
-  //       console.log(response);
-  //     }
-  //   );
-  // };
-
-  // const getJoke = () => {
-  //   fetch("https://official-joke-api.appspot.com/random_joke")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setJoke(`${data.setup} ... ${data.punchline}`);
-  //       console.log(data);
-  //     });
-  // };
-
   return (
     <div className="App">
       {/* <h1 style={{ color: color }}>Hello useState!</h1>
@@ -60,10 +41,7 @@ function App() {
       >
         {count}
       </h1> */}
-
-      {/* <h1>Hello API!</h1> */}
-      {/* <q style={{ display: "block", marginBottom: "1rem" }}>{joke}</q> */}
-      {/* <button onClick={getJoke}>Get Joke!</button> */}
+      <RandomJokeApiCall />
     </div>
   );
 }
