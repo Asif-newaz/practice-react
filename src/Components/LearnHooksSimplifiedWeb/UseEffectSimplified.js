5ffc8682fa48a12de38beaa55392090e2741d6e5
Asif-newaz <asif.newaz0093@gmail.com>
2021-02-17 17:14:12 +0600

    worked with useeffect

--- src/Components/LearnHooksSimplifiedWeb/UseEffectSimplified.js
+++ src/Components/LearnHooksSimplifiedWeb/UseEffectSimplified.js
@@ -4,20 +4,45 @@ import React, { useEffect, useState } from "react";
 import Wrapper from "./UseEffectSimplified.style";
 
 const UseEffectSimplified = () => {
-    const [resourceType, setResourceType] = useState('Posts');
+  const [resourceType, setResourceType] = useState("Posts");
+  // const [item, setItem] = useState([]);
+  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    console.log("resource changed!");
+    return () => {
+      console.log("return from resource change!");
+    };
+  }, [resourceType]);
 
-    useEffect(() => {
-        console.log('Hello useEffect');
-    })
+  // useEffect(() => {
+  //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+  //     .then((response) => response.json())
+  //     .then((json) => setItem(json));
+  // }, [resourceType]);
+
+  // const handleResize = () => {
+  //   setWindowWidth(window.innerWidth);
+  // };
+
+  // useEffect(() => {
+  //   window.addEventListener("resize", handleResize);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
 
   return (
     <Wrapper>
       <div>
-        <button onClick={() => setResourceType('posts')}>Posts</button>
-        <button onClick={() => setResourceType('users')}>Users</button>
-        <button onClick={() => setResourceType('comments')}>Comments</button>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
       </div>
       <h1>{resourceType}</h1>
+      {/* {item.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>
+      })} */}
+      {/* {windowWidth} */}
     </Wrapper>
   );
 };
