c749590fb19aa2d02ef5160141526ab5099b9bec
Asif-newaz <asif.newaz0093@gmail.com>
2021-03-03 13:25:13 +0600

    add new package

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 
 // Components
 // import RandomJokeApiCall from "./Components/RandomJokeApiCall/index";
 // import LearnUseEffectScrimba from "./Components/LearnUseEffectScrimba/index";
 // import UseStateSimplified from "./Components/LearnHooksSimplifiedWeb/UseStateSimplified";
-import UseEffectSimplified from "./Components/LearnHooksSimplifiedWeb/UseEffectSimplified.js";
+// import UseEffectSimplified from "./Components/LearnHooksSimplifiedWeb/UseEffectSimplified.js";
+import ProductDetails from "./Components/ProductDetails/index";
 
 function App() {
   return (
     <div className="App">
-      <UseEffectSimplified />
+      <ProductDetails />
+      {/* <UseEffectSimplified /> */}
       {/* <UseStateSimplified /> */}
       {/* <LearnUseEffectScrimba /> */}
       {/* <RandomJokeApiCall /> */}
