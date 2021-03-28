d8946c021ae4c5af7b26b5b9dfd7c17eda49bf1d
Asif-newaz <asif.newaz0093@gmail.com>
2021-03-28 16:49:36 +0600

    started working on react slick slider
    I added react slick slider. But it's still not working.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import React from "react";
 // import LearnUseEffectScrimba from "./Components/LearnUseEffectScrimba/index";
 // import UseStateSimplified from "./Components/LearnHooksSimplifiedWeb/UseStateSimplified";
 // import UseEffectSimplified from "./Components/LearnHooksSimplifiedWeb/UseEffectSimplified.js";
+import ReactSlick from "./Components/ReactSlick/index";
 
 function App() {
   return (
     <div className="App">
+      <ReactSlick />
       {/* <UseEffectSimplified /> */}
       {/* <UseStateSimplified /> */}
       {/* <LearnUseEffectScrimba /> */}
