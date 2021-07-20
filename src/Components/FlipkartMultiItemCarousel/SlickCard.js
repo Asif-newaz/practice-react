80561d58d9b122ab8ec25cff0a47ef3c82cba47e
Asif-newaz <asif.newaz0093@gmail.com>
2021-07-20 15:25:51 +0600

    update card

--- src/Components/FlipkartMultiItemCarousel/SlickCard.js
+++ src/Components/FlipkartMultiItemCarousel/SlickCard.js
@@ -4,8 +4,8 @@ import { Box } from "@chakra-ui/react";
 
 const SlickCard = (props) => {
   return (
-    <Box height="80vh" w="100%">
-      <Box>
+    <Box height="80vh" w="100%" mr="1rem">
+      <Box mr="1rem" height="80vh" width="100%">
         <img
           src={props.img}
           alt="dh shop"
